48787cc1bf44972977cf6ca02ca8abf6f45c38ed
leon3108 <maxime.noel@epitech.eu>
2024-07-04 09:43:31 +0200

    WIP: use authorization code method

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import PlayerBar from '@/app/common/playerBar/player-bar'
 import SideBar from '@/app/common/sidebar/sidebar'
 import { ThemeProvider } from '@/app/theme-provider'
+import { getTracks } from '@/lib/spotify'
 import {
   ResizableHandle,
   ResizablePanel,
@@ -18,11 +19,28 @@ export const metadata: Metadata = {
   description: 'Clone of Spotify, built with NextJs, by @leon',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const response = await getTracks()
+  const data = await response.json()
+  console.log('data = ', data)
+  const items = data.items
+  if (items) {
+    console.log('items.length = ', items.length)
+    const itemsSliced = items.slice(0, 10)
+    for (const item of itemsSliced) {
+      console.log('playlist name = ', item.name)
+      console.log('playlist owner = ', item.owner.display_name)
+    }
+    // const tracks = items.slice(0, 10).map((track: any) => ({
+    //   artist: track.artists.map((_artist: any) => _artist.name).join(', '),
+    //   songUrl: track.external_urls.spotify,
+    //   title: track.name,
+    // }))
+  }
   return (
     <html lang="en">
       <body className={inter.className}>
